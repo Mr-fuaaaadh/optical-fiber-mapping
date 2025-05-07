8bb11377ade10ac46f7c4a8e72e8bcd7725fea92
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-05-07 17:13:39 +0530

    psql connct

--- opticalfiber/settings.py
+++ opticalfiber/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'opticalfiber.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'optical_db',          
+        'USER': 'postgres',             
+        'PASSWORD': '252562',   
+        'HOST': 'localhost',            
+        'PORT': '5432',               
     }
 }
 
