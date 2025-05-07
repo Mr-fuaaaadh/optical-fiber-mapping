fcf91ece1d8a99167e38b941b9e6f3afee3f9756
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-05-07 17:39:53 +0530

    static

--- opticalfiber/settings.py
+++ opticalfiber/settings.py
@@ -113,8 +113,10 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",  #
 ]
+
+
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
