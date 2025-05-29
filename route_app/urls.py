b7bd5bce3036830c6573647fb90ee9ad61915176
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-05-29 17:32:02 +0530

    fbfhbfdDF

--- route_app/urls.py
+++ route_app/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('add/', FiberRouteView.as_view(), name='route-add'),
     path('list/<int:pk>/', FiberRouteListView.as_view(), name='route-list'),
-    path('management/<int:route_id>/', FiberRouteManagementView.as_view(), name='route-management'),
+    path('management/<int:fiber_route_id>/', FiberRouteManagementView.as_view(), name='route-management'),
     path('management/<int:fiber_route_id>/delete/', FiberRouteManagementView.as_view(), name='route-management-delete'),
     path('management/<int:fiber_route_id>/update/', FiberRouteManagementView.as_view(), name='route-management-update'),
 ]
