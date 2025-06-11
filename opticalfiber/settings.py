2abdb0dcbd4418dfcefde32a9312d7ac98f404d1
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-06-11 15:01:06 +0530

    jbshbsdj

--- opticalfiber/settings.py
+++ opticalfiber/settings.py
@@ -173,7 +173,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # For development
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # For production collectstatic
+
+# Media files (User uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
