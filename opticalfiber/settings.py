8ceead6012cefe8f8ac2ac555339adf521107e6a
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-07-31 11:21:29 +0530

    fjrfef

--- opticalfiber/settings.py
+++ opticalfiber/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8080"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8080","https://optical-fiber-mapping.vercel.app"]
 CORS_ALLOW_CREDENTIALS = True
 
 
