bedd88e242592ff4cbd168018fb6cbb88d828eeb
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-08-08 11:08:08 +0530

    efefweFEF

--- opticalfiber/settings.py
+++ opticalfiber/settings.py
@@ -110,28 +110,28 @@ WSGI_APPLICATION = 'opticalfiber.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'optical_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '252562',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'optical_db',
-        'USER': 'optical_user',
+        'USER': 'postgres',
         'PASSWORD': '252562',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'optical_db',
+#         'USER': 'optical_user',
+#         'PASSWORD': '252562',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 STATIC_URL = '/static/'
@@ -244,6 +244,3 @@ REST_FRAMEWORK = {
 
 import os
 
-CASHFREE_APP_ID = os.getenv('CASHFREE_APP_ID')
-CASHFREE_SECRET_KEY = os.getenv('CASHFREE_SECRET_KEY')
-CASHFREE_WEBHOOK_SECRET = os.getenv('CASHFREE_WEBHOOK_SECRET')
