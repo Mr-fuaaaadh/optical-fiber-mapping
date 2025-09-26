77fcba22509d2f448785f7b40f8450cfed951f6b
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-09-26 14:43:34 +0530

    change passwordv

--- opticalfiber_app/urls.py
+++ opticalfiber_app/urls.py
@@ -7,11 +7,10 @@ urlpatterns = [
     path('company/staffs/', ListAllStaffByCompany.as_view(),name="staffs"),
     path('update/user/profile/',EditStaffProfile.as_view(),name="edit_profile"),
 
-    path('change/password/', ChangePasswordView.as_view(), name='change_password'), # change password when logged in
-
     path('forgot/password/',ForgotPasswordView.as_view(),name="forgot_password"),
     path('verify/otp/',VerifyOTPView.as_view(),name="verify_otp"),
     path('reset/password/',ResetPasswordView.as_view(),name="reset_password"),
+    path('change/password/', ChangePasswordView.as_view(), name='change_password'),
 
 
 
